b9daa2d1f2580bad9e0ba64faae72a0964e2d3fd
searchsakib <searchsakib@gmail.com>
2023-12-12 19:58:21 +0600

    about & contact page added

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="bg-red-700">
+      <h2 className="text-green-500 bg-red-400">About Page is Here</h2>
+    </div>
+  );
+};
+
+export default AboutPage;
