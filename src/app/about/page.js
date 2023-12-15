9301440ebf1d6956d5957de9c104392e5e68e1ad
searchsakib <searchsakib@gmail.com>
2023-12-15 18:31:27 +0600

    about path background changed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div className="bg-red-700">
-      <h2 className="text-green-500">About Page is Here</h2>
+    <div className="h-screen bg-gray-800">
+      <h2 className="text-5xl text-gray-300 text-center p-10 font-medium">
+        About Page is Here
+      </h2>
     </div>
   );
 };
