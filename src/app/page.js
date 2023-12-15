8412b284e0d9bc4b04550d42121847193e235a4a
searchsakib <searchsakib@gmail.com>
2023-12-15 23:13:43 +0600

    changed the bg of home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-5xl text-center text-violet-500 font-medium">
-        This is next app
+    <div className="h-screen bg-gray-700">
+      <h2 className="text-5xl text-center text-sky-300 font-medium p-10">
+        This is Home Page
       </h2>
     </div>
   );
