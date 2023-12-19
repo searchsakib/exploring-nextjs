529197bbf5f94bc40b80de0fd58fe71e2a23b0b6
searchsakib <searchsakib@gmail.com>
2023-12-19 15:32:32 +0600

    73-6 video finished

---
+++ src/app/News/Blog/page.js
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl text-pink-400 text-center">This is Blog Page</h2>
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ src/app/News/page.js
@@ -0,0 +1,11 @@
+const NewsPage = () => {
+  return (
+    <div>
+      <h2 className="text-center text-2xl text-yellow-400">
+        This is News Page
+      </h2>
+    </div>
+  );
+};
+
+export default NewsPage;
