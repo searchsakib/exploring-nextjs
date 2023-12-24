e3ebd096137f12ef0ffff393f5d1bf7caed05212
searchsakib <searchsakib@gmail.com>
2023-12-24 19:52:42 +0600

    succesfully created error.js page & error throwed through News page

---
+++ src/app/error.js
@@ -0,0 +1,13 @@
+'use client';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2 className="text-red-500 text-center text-3xl pt-32">
+        This is General Error
+      </h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
